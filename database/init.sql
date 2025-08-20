51ce576919fe971aab3eb6a7fb841ef485dc4501
Istomin Alexey <tmozzze@gmail.com>
2025-08-21 00:17:07 +0300

    ADDED: docker-compose for postgres. init.sql to create tables

---
+++ database/init.sql
@@ -0,0 +1,58 @@
+-- database/init.sql
+
+CREATE TABLE orders (
+    order_uid TEXT PRIMARY KEY NOT NULL,
+    track_number TEXT,
+    entry TEXT,
+    locale TEXT,
+    internal_signature TEXT,
+    customer_id TEXT NOT NULL,
+    delivery_service TEXT,
+    shardkey TEXT,
+    sm_id INT,
+    date_created TIMESTAMP,
+    oof_shard TEXT
+);
+
+CREATE TABLE deliveries (
+    id SERIAL PRIMARY KEY NOT NULL,
+    order_uid TEXT REFERENCES orders(order_uid),
+    name TEXT,
+    phone TEXT,
+    zip TEXT,
+    city TEXT,
+    address TEXT,
+    region TEXT,
+    email TEXT
+);
+
+CREATE TABLE payments (
+    id SERIAL PRIMARY KEY NOT NULL,
+    order_uid TEXT REFERENCES orders(order_uid),
+    transaction TEXT,
+    request_id TEXT,
+    currency TEXT,
+    provider TEXT,
+    amount INT,
+    payment_dt BIGINT,
+    bank TEXT,
+    delivery_cost INT,
+    goods_total INT,
+    custom_fee INT
+);
+
+CREATE TABLE items (
+    id SERIAL PRIMARY KEY NOT NULL,
+    order_uid TEXT REFERENCES orders(order_uid),
+    chrt_id INT,
+    track_number TEXT,
+    price INT,
+    rid TEXT,
+    name TEXT,
+    sale INT,
+    size TEXT,
+    total_price INT,
+    nm_id INT,
+    brand TEXT,
+    status INT
+);
