3e97b2548ddf16dd87bb25beaaa491c2581a7b5b
Istomin Alexey <tmozzze@gmail.com>
2025-08-29 21:58:49 +0300

    UPDATE: consumers logs

--- internal/kafka_consumer/consumer.go
+++ internal/kafka_consumer/consumer.go
@@ -19,34 +19,58 @@ type Consumer struct {
 
 func NewConsumer(brokers []string, topic, groupID string, repo *repository.OrderRepository, c *cache.Cache) *Consumer {
 	r := kafka.NewReader(kafka.ReaderConfig{
-		Brokers: brokers,
-		Topic:   topic,
-		GroupID: groupID,
+		Brokers:           brokers,
+		Topic:             topic,
+		GroupID:           groupID,
+		MinBytes:          1,
+		MaxBytes:          10e6,
+		HeartbeatInterval: 3_000_000_000,  //3s
+		SessionTimeout:    30_000_000_000, //30s
+		CommitInterval:    1_000_000_000,  //1s
+
 	})
 
 	return &Consumer{reader: r, repo: repo, cache: c}
 }
 
 func (c *Consumer) Start(ctx context.Context) error {
+	log.Println("Kafka consumer started")
+
+	// Exit if ctx cancel
 	for {
+		select {
+		case <-ctx.Done():
+			log.Println("Kafka consumer stopping...")
+			if err := c.reader.Close(); err != nil {
+				log.Printf("failed to close kafka reader: %v", err)
+			}
+			return nil
+		default:
+		}
+
 		m, err := c.reader.ReadMessage(ctx)
 		if err != nil {
 			return err
 		}
 
+		log.Printf("got message")
+
+		// Parsing order
 		var order models.Order
 		if err := json.Unmarshal(m.Value, &order); err != nil {
 			log.Printf("invalid message: %v", err)
 			continue
 		}
 
+		// Saving to postgres
 		if err := c.repo.SaveOrder(ctx, &order); err != nil {
 			log.Printf("invalid message: %v", err)
 			continue
 		}
 
+		// Cache
 		c.cache.Set(order.OrderUID, &order)
-		log.Printf("order %s processed", order.OrderUID)
+		log.Printf("order cached: %s", order.OrderUID)
 
 	}
 }
